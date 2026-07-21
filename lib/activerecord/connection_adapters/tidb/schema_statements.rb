4597b6185fbca67fa50fd25246ab20e91212e301
myoan <motoki.yoan@gmail.com>
2026-07-21 10:28:15 +0900

    :fix: Rebuild composite indexes around DROP COLUMN
    TiDB rejects DROP COLUMN when the column is part of a multi-column
    index. remove_column/remove_columns now drop and re-add the affected
    composite indexes (minus the removed column) around the column drop
    instead of letting the error surface.

--- lib/activerecord/connection_adapters/tidb/schema_statements.rb
+++ lib/activerecord/connection_adapters/tidb/schema_statements.rb
@@ -13,6 +13,58 @@ module ActiveRecord
         def valid_table_definition_options # :nodoc:
           super + [:auto_id_cache, :clustered, :pre_split_regions, :shard_row_id_bits]
         end
+
+        # TiDB refuses to DROP COLUMN a column that is part of a composite
+        # (multi-column) index: "MySQL::Error: can't drop column to with
+        # composite index covered or Primary Key covered now", unlike
+        # MySQL/InnoDB which just narrows the index. Work around it by
+        # dropping the affected composite indexes before the DROP COLUMN
+        # and re-adding them without the removed column(s) afterwards.
+        def remove_column(table_name, column_name, type = nil, **options)
+          rebuild_composite_indexes_without_columns(table_name, [column_name]) do
+            super
+          end
+        end
+
+        def remove_columns(table_name, *column_names, type: nil, **options)
+          rebuild_composite_indexes_without_columns(table_name, column_names) do
+            super
+          end
+        end
+
+        private
+          def rebuild_composite_indexes_without_columns(table_name, column_names)
+            dropped = column_names.map(&:to_s)
+
+            affected_indexes = indexes(table_name).select do |index|
+              index.columns.is_a?(Array) &&
+                index.columns.size > 1 &&
+                (index.columns.map(&:to_s) & dropped).any?
+            end
+
+            affected_indexes.each { |index| remove_index(table_name, name: index.name) }
+
+            result = yield
+
+            affected_indexes.each do |index|
+              remaining_columns = index.columns.reject { |column| dropped.include?(column.to_s) }
+              next if remaining_columns.empty?
+
+              index_options = { name: index.name, unique: index.unique }
+              index_options[:using] = index.using if index.using
+              index_options[:comment] = index.comment if index.comment
+
+              lengths = index.lengths.slice(*remaining_columns)
+              index_options[:length] = lengths if lengths.present?
+
+              orders = index.orders.slice(*remaining_columns)
+              index_options[:order] = orders if orders.present?
+
+              add_index(table_name, remaining_columns, **index_options)
+            end
+
+            result
+          end
       end
     end
   end
--- test/activerecord/tidb/adapter_test.rb
+++ test/activerecord/tidb/adapter_test.rb
@@ -69,6 +69,17 @@ class AutoRandomInColumn < ActiveRecord::Migration[7.2]
   end
 end
 
+class TableWithCompositeIndex < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string "first_name"
+      t.string "last_name"
+      t.string "email"
+    end
+    add_index :users, [:first_name, :last_name], name: "index_users_on_first_name_and_last_name"
+  end
+end
+
 class Activerecord::Tidb::AdapterTest < Minitest::Test
   attr_reader :connection
 
@@ -198,5 +209,42 @@ class Activerecord::Tidb::AdapterTest < Minitest::Test
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
     QUERY
     assert_equal expected_query, query
+  end
+
+  # TiDB rejects `DROP COLUMN` for a column that is part of a composite
+  # index ("can't drop column ... with composite index covered ... now").
+  # remove_column must rebuild the index without the dropped column instead
+  # of letting that error bubble up.
+  def test_remove_column_rebuilds_composite_index_without_dropped_column
+    migrate(TableWithCompositeIndex)
+
+    connection.remove_column :users, :first_name
+
+    refute connection.column_exists?(:users, :first_name)
+    index = connection.indexes(:users).find { |i| i.name == "index_users_on_first_name_and_last_name" }
+    refute_nil index
+    assert_equal ["last_name"], index.columns
+  end
+
+  def test_remove_columns_rebuilds_composite_index_without_dropped_columns
+    migrate(TableWithCompositeIndex)
+
+    connection.remove_columns :users, :first_name, :email
+
+    refute connection.column_exists?(:users, :first_name)
+    refute connection.column_exists?(:users, :email)
+    index = connection.indexes(:users).find { |i| i.name == "index_users_on_first_name_and_last_name" }
+    refute_nil index
+    assert_equal ["last_name"], index.columns
+  end
+
+  def test_remove_column_drops_index_entirely_when_no_columns_remain
+    migrate(TableWithCompositeIndex)
+
+    connection.remove_column :users, :first_name
+    connection.remove_column :users, :last_name
+
+    index = connection.indexes(:users).find { |i| i.name == "index_users_on_first_name_and_last_name" }
+    assert_nil index
   end
 end
